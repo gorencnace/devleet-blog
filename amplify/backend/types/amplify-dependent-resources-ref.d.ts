d88bff8379cdbc0eadcbac2c83e7fc1b7ef1f466
gorencnace <nace.gorenc@gmail.com>
2022-02-08 17:01:34 +0100

    ADD/UPDATE: Amplify files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "devleetproject518d6dce": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
