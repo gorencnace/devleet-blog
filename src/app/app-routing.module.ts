f433d289ec26dabd1ff651cc5a410f44697a00c7
gorencnace <nace.gorenc@gmail.com>
2022-02-08 16:51:14 +0100

    ADD: new routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommentCreateComponent } from './comment-create/comment-create.component';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PostsComponent},
+  {path: 'post-details/:id', component: PostDetailsComponent},
+  {path: 'post-create', component: PostCreateComponent},
+  {path: 'post-details/:id/comment-create', component: CommentCreateComponent},
+  {path: 'comment/:id/comment-reply', component: CommentCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
