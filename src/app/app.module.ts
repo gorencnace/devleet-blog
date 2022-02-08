0afbc5296e194548b9d6b20c97091baa4f2f14a4
gorencnace <nace.gorenc@gmail.com>
2022-02-08 17:00:56 +0100

    UPDATE: mostly imports in app files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+/* import AmplifyUIAngularModule*/
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
+import { CommentComponent } from './comment/comment.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { CommentCreateComponent } from './comment-create/comment-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostDetailsComponent,
+    CommentComponent,
+    PostCreateComponent,
+    CommentCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyUIAngularModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
